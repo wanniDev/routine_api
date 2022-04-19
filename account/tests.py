64f7ef4ce4c533e3f948c47342c84d1be4c0e433
wanniDev <wannidev0928@gmail.com>
2022-04-20 01:51:06 +0900

    Test: 사용자 테스트코드 작성 및 requirements.txt 작성

--- account/tests.py
+++ account/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+import json
 
-# Create your tests here.
+class UserTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_sigh_up_with_wrong_password_format(self) :
+        payload = {'email':'test@test.com', 'password':'a123'}
+        jLoad = json.dumps(payload);
+        response = self.client.post('http://localhost:8000/account/signup/', jLoad, content_type="application/x-www-form-urlencoded")
+        self.assertNotEqual(response.status_code, 200)
--- routine_api/settings.py
+++ routine_api/settings.py
@@ -16,10 +16,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 def env_to_bool(env, default):
     str_val = os.environ.get(env)
