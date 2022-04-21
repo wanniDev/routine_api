2bb2a0ff67c50ba7ff6502685e87759ae9eb4bcd
wanniDev <wannidev0928@gmail.com>
2022-04-21 21:48:51 +0900

    Feat: routine 목록 view

--- routine/urls.py
+++ routine/urls.py
@@ -7,5 +7,6 @@ app_name = 'routine'
 
 urlpatterns = [
     path('create/', views.CreateRoutineView().as_view()),
-    path('get/', views.RoutineView().as_view())
+    path('get/', views.RoutineView().as_view()),
+    path('list/', views.RoutineListView().as_view())
 ]
